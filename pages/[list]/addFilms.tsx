571dd22c0e8f2c9c16c16e60c5bdb454ce714b2a
Jean-Paul <Jean.paul.bassil@outlook.com>
2023-05-12 00:47:08 +0300

    added styles for the last page

--- pages/[list]/addFilms.tsx
+++ pages/[list]/addFilms.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import Styles from "../../styles/newFilms.module.sass";
 import { GetServerSideProps } from "next";
+import Globals from "../../styles/globals.module.sass";
 
 interface FilmList {
   id: number;
@@ -90,7 +91,6 @@ const AddFilms: React.FC<FilmProps> = ({ film }) => {
   const handleCategoryClick = (category: string) => {
     setSelectedCategory(category);
     setShowDropdown(false);
-    router.push('/')
   };
 
   
@@ -103,82 +103,125 @@ const AddFilms: React.FC<FilmProps> = ({ film }) => {
   console.log(disabledButtons)
   return (
     <div className={Styles.container}>
-      <h1>Add films to the Favorites List: {film.title}</h1>
-      <div className={Styles.search}>
-        <div className={Styles.input}>
-          <div className={Styles.inputs}>
-            <p>Actor Name</p>
-            <input
-              placeholder="Name"
-              type="text"
-              value={actorName}
-              onChange={(e) => setActorName(e.target.value)}
-            />
-            <button onClick={searchFilms} className={Styles.enabled}>
-              <p>Search Actors</p>
-            </button>
-          </div>
-          <div className={Styles.inputs}>
-            <p>Film Title</p>
-            <input
-              placeholder="Title"
-              type="text"
-              value={filmTitle}
-              onChange={(e) => setFilmTitle(e.target.value)}
-            />
-            <button onClick={searchFilms} className={Styles.enabled}>
-              <p>Search Titles</p>
-            </button>
-          </div>
-        </div>
-        <div className={Styles.dropdownWrapper}>
-          <button onClick={toggleDropdown} className={Styles.enabled}>{selectedCategory}</button>
-          {showDropdown && (
-            <div className={Styles.dropdown}>
-              <button onClick={() => handleCategoryClick("Select category")} className={Styles.enabled}>
-                <p>Remove Category</p>
-              </button>
-              {categories.map((category) => (
-                <button
-                  key={category}
-                  onClick={() => handleCategoryClick(category)}
-                  className={Styles.enabled}
-                >
-                  <p>{category}</p>
-                </button>
-              ))}
+      <div className={Styles.home}>
+        <h1>Add films to the Favorites List: {film.title}</h1>
+        <div className={Styles.search}>
+          <div className={Styles.input}>
+            <div className={Styles.inputs}>
+              <div className={Styles.formControl}>
+                <input
+                  title="name"
+                  type="value"
+                  value={actorName}
+                  onChange={(e) => setActorName(e.target.value)}
+                  placeholder=" "
+                />
+
+                <label>
+                  <span style={{transitionDelay: "0ms"}}>A</span>
+                  <span style={{transitionDelay: "50ms"}}>c</span>
+                  <span style={{transitionDelay: "100ms"}}>t</span>
+                  <span style={{transitionDelay: "150ms"}}>o</span>
+                  <span style={{transitionDelay: "200ms"}}>r</span>
+                  <span style={{transitionDelay: "250ms"}}> </span>
+                  <span style={{transitionDelay: "300ms"}}>N</span>
+                  <span style={{transitionDelay: "350ms"}}>a</span>
+                  <span style={{transitionDelay: "400ms"}}>m</span>
+                  <span style={{transitionDelay: "450ms"}}>e</span>
+                </label>
+                
+              </div>
+              
             </div>
-          )}
+            <div className={Styles.inputs}>
+              <div className={Styles.formControl}>
+                <input
+                  title="title"
+                  type="value"
+                  value={filmTitle}
+                  onChange={(e) => setFilmTitle(e.target.value)}
+                  placeholder=" "
+                />
+
+                <label>
+                  <span style={{transitionDelay: "0ms"}}>F</span>
+                  <span style={{transitionDelay: "50ms"}}>i</span>
+                  <span style={{transitionDelay: "100ms"}}>l</span>
+                  <span style={{transitionDelay: "150ms"}}>m</span>
+                  <span style={{transitionDelay: "200ms"}}></span>
+                  <span style={{transitionDelay: "250ms"}}>T </span>
+                  <span style={{transitionDelay: "300ms"}}>i</span>
+                  <span style={{transitionDelay: "350ms"}}>t</span>
+                  <span style={{transitionDelay: "400ms"}}>l</span>
+                  <span style={{transitionDelay: "450ms"}}>e</span>
+                </label>
+              </div>
+              
+            </div>
+              
+          </div>
+          <div className={Styles.dropdownWrapper}>
+            <button onClick={toggleDropdown} className={Styles.btn}>
+            <span className={Styles.icon}>
+              <svg viewBox="0 0 175 80" width="40" height="40">
+                  <rect width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+                  <rect y="30" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+                  <rect y="60" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+              </svg>
+          </span>
+              <span className={Styles.text}>{selectedCategory}</span>
+            </button>
+            {showDropdown && (
+              <ul className={Styles.dropdown}>
+                <button onClick={() => handleCategoryClick("Select category")} className={Styles.enabled}>
+                  <p>Remove Category</p>
+                </button>
+                {categories.map((category) => (
+                  <button
+                    key={category}
+                    onClick={() => handleCategoryClick(category)}
+                    className={Styles.enabled}
+                  >
+                    <p>{category}</p>
+                  </button>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-      <div className={Styles.lower}>
-        <button className={Styles.close} onClick={() => router.push('/')}>
-          <p>close</p>
-        </button>
-        <div className={Styles.output}>
-          <ul>
-            {fetchedFilms.length === 0
-              ? Array(5)
-                  .fill(null)
-                  .map((_, index) => (
-                    <li key={index}>
-                      <button disabled>
-                        <p>---</p>
+        <div className={Styles.lower}>
+          <div className={Styles.buttons}>
+            <button className={Styles.close} onClick={() => router.push('/')} className={Globals.darkBtn}>
+              <p>close</p>
+            </button>
+            <button onClick={searchFilms} className={Globals.darkBtn}>
+                  <p>Search</p>
+            </button>
+          </div>
+          <div className={Styles.output}>
+            <ul>
+              {fetchedFilms.length === 0
+                ? Array(5)
+                    .fill(null)
+                    .map((_, index) => (
+                      <li key={index}>
+                        <button disabled className={Styles.disabled}>
+                          <p>---</p>
+                        </button>
+                      </li>
+                    ))
+                : fetchedFilms.map((filmData, index) => (
+                    <li key={filmData.id}>
+                      <button
+                        onClick={() => handleListButtonClick(index)}
+                        className={disabledButtons[index] ? Styles.enabled : Styles.disabled}
+                      >
+                        <p>{filmData.title}</p>
                       </button>
                     </li>
-                  ))
-              : fetchedFilms.map((filmData, index) => (
-                  <li key={filmData.id}>
-                    <button
-                      disabled={!disabledButtons[index]}
-                      onClick={() => handleListButtonClick(index)}
-                      className={disabledButtons[index] ? Styles.enabled : ""}
-                    >
-                      <p>{filmData.title}</p>
-                    </button>
-                  </li>
-                ))}
-          </ul>
+                  ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
